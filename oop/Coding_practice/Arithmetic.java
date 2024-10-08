fc96a86bdfbfc41f22c3abf1455b4c9533e9187c
D R Jones <davecastlejones10@gmail.com>
2024-10-08 14:39:40 +0100

    did some java programming

---
+++ oop/Coding_practice/Arithmetic.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;// the java modulue?? that is used for taking inputs from the terminal/command line
+
+public class Arithmetic
+{
+    public static void main(String[] args)
+    {
+        Scanner inputDevice = new Scanner(System.in);
+
+        System.out.println("Please enter a number");
+        int firstNumber = inputDevice.nextInt();
+        inputDevice.nextLine();
+        System.out.println("You chose " + firstNumber + " as your first number");
+        
+        System.out.println("Please enter a second number");
+        int secondNumber = inputDevice.nextInt();
+        inputDevice.nextLine();
+        System.out.println("You chose " + secondNumber + " as your first number");
+
+        int sum = firstNumber + secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        int dividionFirstBySecond = firstNumber / secondNumber;
+        int divisionSecondByFirst = secondNumber / firstNumber;
+
+        System.out.println(firstNumber + "+" +  secondNumber + "=" + sum);
+        System.out.println(firstNumber + "*" +  secondNumber + "=" + multiplication);
+        System.out.println(firstNumber + "/" +  secondNumber + "=" + dividionFirstBySecond);
+        System.out.println(secondNumber + "/" +  firstNumber + "=" + divisionSecondByFirst);
+    }
+}
+        
---
+++ oop/Coding_practice/ExtraPractice.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class ExtraPractice 
+{
+
+    public static void main(String[] args)
+    {
+        Scanner inputDevice = new Scanner(System.in);
+
+        System.out.println("Enter Your Age");
+        String myAge = inputDevice.nextLine();
+
+        int age = Integer.parseInt(myAge);
+
+        System.out.println("You are :" + age);
+
+
+    }
+    
+}
