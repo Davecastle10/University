7b86b8b7296ecb95919a6f1eae36e3562d79cd2b
D R Jones <davecastlejones10@gmail.com>
2024-10-10 11:01:09 +0100

    did the week 1 oop java homework

---
+++ oop/Coding_practice/ParseTest.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.time.Period;
+
+public class ParseTest 
+{
+    public static void main(String[] args)
+    {
+        String time;// time will from a string of from "PaYbMcD" where a = num years, b = num months, and c = num days
+        Scanner inputDevice = new Scanner(System.in);
+
+
+        System.out.println("Enter A Number Of Years");
+        time = "P" + inputDevice.nextLine() +"Y";
+
+        System.out.println("Enter A Number Of Months");
+        time = time + inputDevice.nextLine() + "M";
+
+        System.out.println("Enter A Number Of Days");
+        time = time + inputDevice.nextLine() + "D";
+        
+        Period p = Period.parse(time);// the Period object p cn nparse the data from the weird string, to provide a value for years, months, and days
+
+        System.out.println("The Time Is:");
+        System.out.println(p.getYears() + " Years\n");
+        System.out.println(p.getMonths() + " Months\n");
+        System.out.println(p.getDays() + " Days\n");
+
+
+    }
+    
+}
