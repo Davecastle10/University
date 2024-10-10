a8b8bc5506d078427af85567243e000a3addab28
D R Jones <davecastlejones10@gmail.com>
2024-10-10 11:24:38 +0100

    did some of the week 1 challenge work

---
+++ oop/Coding_practice/StoringNumbersDatatypeTesting.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+import java.time.Period;
+
+
+public class StoringNumbersDatatypeTesting {
+    
+    public static void main(String[] args)
+    {
+        String time;// time will from a string of from "PaYbMcD" where a = num years, b = num months, and c = num days
+        Scanner inputDevice = new Scanner(System.in);
+        int testing1 = 2141483647;
+        int testing2 = 2141483647;
+        
+        double posInf = Double.POSITIVE_INFINITY;
+        double negInf = Double.NEGATIVE_INFINITY;
+
+
+        System.out.println(testing1 + testing2);
+        System.out.println(posInf + negInf);
+        System.out.println(posInf + posInf);
+        System.out.println(negInf + negInf);
+
+    }
+    
+}
