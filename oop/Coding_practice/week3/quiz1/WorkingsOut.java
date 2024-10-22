544591ec1faab3acc3f16c33bc8d41e14a7f949a
D R Jones <davecastlejones10@gmail.com>
2024-10-22 14:42:25 +0100

    ran some code for the comp sci quiz 1

---
+++ oop/Coding_practice/week3/quiz1/WorkingsOut.java
@@ -0,0 +1,14 @@
+package week3.quiz1;
+
+import java.util.Scanner;
+
+public class WorkingsOut {
+    public static void main(String[] args)
+    {
+        int x = 10
+        int y = 0;
+        System.out.println("Result: " + (x / y));
+    
+
+    }
+}
