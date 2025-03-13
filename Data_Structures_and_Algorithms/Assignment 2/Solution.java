053e7754b149eaf690443b7d45c9d38f432654e0
D R Jones <davecastlejones10@gmail.com>
2025-03-13 15:47:55 +0000

    1a working but 1b not for dsa assignment 2

--- Data_Structures_and_Algorithms/Assignment 2/Solution.java
+++ Data_Structures_and_Algorithms/Assignment 2/Solution.java
@@ -1,5 +1,4 @@
 public class Solution {
-    
     // Question 1a
     public static boolean reachable1(String x, String y) {
         // operations require 1 and 0 for each operatoion
@@ -9,21 +8,35 @@ public class Solution {
         int num1Y = 0;
         for (int i = 0; i < x.length(); i++)
         {
-            if (x.charAt(i) == 1)
+            if (x.charAt(i) == '1')
             {
-                num1X += 1;
+                num1X +=  1;
             }
-            if (y.charAt(i) == 1)
+            if (y.charAt(i) == '1')
             {
-                num1Y += 1;
+                num1Y +=  1;
             }
         }
-        return num1X == num1Y;
+        return num1X == num1Y;// time complexity O(n) as for loop use to iterate through both strings technically 2n + 1 comparisons, + 2n + 2 assignments max
+        // space complexity constant O(1) as only have two vairbales assigned outside of intial state.
     }
 
     // Question 1b
     public static int distance1(String x, String y) {
-        return -1000000;
+
+        if (!reachable1(x, y))
+        {
+            return -1;
+        }
+        int numDifferences = 0;
+        for (int i = 0; i < x.length(); i++)
+        {
+            if (x.charAt(i) == y.charAt(i))
+            {
+                numDifferences +=  1;
+            }
+        }
+        return numDifferences;
     }
 
     // Question 2
