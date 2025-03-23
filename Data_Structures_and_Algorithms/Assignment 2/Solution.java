aba8d302d59839b917640bca038827be9bf6685a
D R Jones <davecastlejones10@gmail.com>
2025-03-23 06:17:46 +0000

    works wel now, but this probaly would have been easier if i remebered that you cna create a class within a class, so i could have made e.g. key value class for string and op# insted of array with type conversions

--- Data_Structures_and_Algorithms/Assignment 2/Solution.java
+++ Data_Structures_and_Algorithms/Assignment 2/Solution.java
@@ -110,12 +110,11 @@ public class Solution {
 
             for (int i = 0; i < x.length() -2; i++)// changed back to -2 realised i was pottentially missing last index
             {
-                char[] xArray = currentXString.toCharArray();
+                char[] xArray = currentXString.toCharArray();// char array to make changing the string easier
                 List<String> newX = null;
-                
-
                 String substringToCheck = currentXString.substring(i, i + 3);
 
+                // checking the current substring against the three operation types to see if one can be performed and if so it performs the operation
                 if (substringToCheck.equals("110"))
                 {
                     xArray[i] = '0';
@@ -141,7 +140,7 @@ public class Solution {
                     xArray[i + 2] = '0';
                     newX = new ArrayList<>();
                     newX.add(new String(xArray));
-                    newX.add( String.valueOf(currentOperationsCount + 1));
+                    newX.add(String.valueOf(currentOperationsCount + 1));
                 }
 
                 // if newX has not already been visited and an operation was applied to create a newX
@@ -153,15 +152,18 @@ public class Solution {
                         return currentOperationsCount + 1;
                     }
 
+                    // implicit else on adding the newX node to the visited array and queue so ti can be iterated on later to see if any of it's 
+                    // permutations can have an operation used on them to create string y.
                     visited.add(newX);
                     queue.add(newX);
                 }
 
             }
         }
-        return -1;
+        return -1;// returns -1 if both loops have exited after all possible permutations have been checked and the only other possibility is that x cannot be made into y
     }
 
+    // time complexity O(n) space O(1) as it doesn't make new stuff
     public static int inGraph(String x, List<List<String>> graph)// should probably change this to in visited but can't really be bothered atm
     {
         for (int i = 0; i < graph.size(); i++)
