837fd13510770f6054cb61f3ec8b3505447d5317
D R Jones <davecastlejones10@gmail.com>
2025-10-15 16:57:29 +0100

    fp haskell problem sheet 2

---
+++ Functional Programming/test.hs
@@ -0,0 +1,20 @@
+double :: Int -> Int
+double x = x + x
+
+orB :: Bool -> Bool -> Bool
+orB a b | a == True = True
+        | b == True = True
+        | otherwise = False
+
+swap :: (a,b) -> (b,a)
+swap (x, y) = (y, x)
+
+unwrap' :: [a] -> [a]
+unwrap' [] = []
+unwrap' xs = reverse(tail(reverse(tail xs)))
+
+reverseLength :: Int -> [a] -> [a]
+reverseLength x ys = if length(ys) > x then reverse(ys) else ys
+
+doubleAndTen :: [Int] -> [Int]
+doubleAndTen
