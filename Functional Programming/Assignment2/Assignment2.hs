9b5eebd117ee200c1b70b0d8e43de34c8a3df438
D R Jones <davecastlejones10@gmail.com>
2025-11-13 10:41:48 +0000

    fixed issues with encodeWord

--- Functional Programming/Assignment2/Assignment2.hs
+++ Functional Programming/Assignment2/Assignment2.hs
@@ -22,20 +22,54 @@ import Types (morseCode, morseTable)
 
 {- Question 1 -}
 encodeWord :: Table -> String -> Code
-encodeWord tableIn xs = concat [case lookup x tableIn of 
-                                        Just code -> code
+
+encodeWord tableIn xs = take (retCodeLength - 2) retCode
+    where
+        retCode = concat [case lookup x tableIn of 
+                                        Just code -> code ++ [Silence,Silence]
                                         Nothing -> [] | x <- xs] -- i don't like the way this looks but also don't want to add another function will will slow down code
+        retCodeLength = length retCode
+
+{-}
+encodeWord tableIn xs = take (retCodeLength - 2) retCode
+    where
+        retCode = [snd (head (filter (\x -> fst x == y) tableIn)) ++ [Silence,Silence] | y <- xs]
+        retCodeLength = length retCode
+-}
 
 encodeWords :: Table -> [String] -> Code
-encodeWords tableIn xs = concat [encodeWord morseTable x | x <- xs]
+encodeWords tableIn xs 
+    | null xs = []
+    -- | otherwise = take (codeListLength - 6) codeList
+    | otherwise = codeList
+    where
+        codeList = concat [encodeWord morseTable x ++ [Silence,Silence,Silence,Silence,Silence,Silence]| x <- xs]
+        codeListLength = length codeList
 
+{-}
 split :: Eq a => [a] -> [a] -> [[a]]
 split delim xs = case break (==delim) xs of 
     (front, []) -> front -- there is no seperatror in the list/string/whatever so just return the front = whole list
     (front, rest) -> front ++ split delim (drop (length delim) rest) -- migth not need the [] around front here if the base case when nothing left sorts it out, but test later.
+-}
+
+{-}
+splitCharString :: Eq a => a -> [a] -> [[a]]
+splitCharString delim xs = case break (==delim) xs of 
+    (front, []) -> front -- there is no seperatror in the list/string/whatever so just return the front = whole list
+    (front, rest) -> front ++ splitCharString delim (drop 1 rest) -- migth not need the [] around front here if the base case when nothing left sorts it out, but test later.
+-}
+
+-- alt take length delim if it is delim, then drop delim
+
+--split :: Eq a => [a] -> [a] -> [[a]]
+
+
+
 
 encodeText :: Table -> String -> Code
-encodeText = undefined
+encodeText tableIn stringIn = encodeWords tableIn (words stringIn)
+
 
 {- Question 2 -}
 decodeText :: Table -> Code -> String
--- Functional Programming/Assignment2/random.hs
+++ Functional Programming/Assignment2/random.hs
@@ -1,1 +1,5 @@
+import Data.List
+
 test1 = [[1,2,3],[2,3,4]]
+
+testFunc = splitOn ['a'] ['b','a','c']
